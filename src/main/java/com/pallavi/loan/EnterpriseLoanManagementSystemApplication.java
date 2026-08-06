2128ba6247c68db66418e0e055849275e78fda87
pallavinemade <pallavi.nemade04@gmail.com>
2026-08-05 22:09:58 -0500

    Initialize Spring Boot Loan Management System

---
+++ src/main/java/com/pallavi/loan/EnterpriseLoanManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pallavi.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseLoanManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseLoanManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pallavi/loan/EnterpriseLoanManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pallavi.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseLoanManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
